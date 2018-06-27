b98b573b544e0e50231fc5e334fb7fbe1362f67f
KhTimur <khazirov@yandex.ru>
2018-06-27 13:41:58 +0300

    Create testsortquick.cpp

---
+++ tests/testsortquick.cpp
@@ -0,0 +1,28 @@
+#include "catch.h"
+#include <algorithm>
+#include <quick_sort.h>
+
+
+SCENARIO("SORT"){
+    int nums[] = { 1,2,3,4,5,6,7,8,9,10 };
+    int numscheck[] = { 1,2,3,4,5,6,7,8,9,10 };
+    int *begi = std::begin(nums);
+    int *en = std::end(nums);
+    long int j=0;
+    for(long int i=0;i<3628800;i++)
+    {
+        std::next_permutation(begi,en);
+        quick_sort(begi, en);
+        int k=1;
+        for (int o = 0; o <= 10; ++o) {
+		if(nums[o] != numscheck[o])
+        {
+         k=0;
+        }
+        j=j+k;
+	}
+
+    }
+    REQUIRE(j==3628800);
+    return 0;
+}
