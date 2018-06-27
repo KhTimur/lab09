2538d3506f6f1c5f7113bc5d11d9949210df74a8
KhTimur <khazirov@yandex.ru>
2018-06-27 13:38:25 +0300

    Update inser.h

--- include/inser.h
+++ include/inser.h
@@ -1,1 +1,11 @@
-void inser(int * begi, int * en);
+#include <iterator>
+
+template <typename RandomAccessIterator>
+void inser(RandomAccessIterator  begi, RandomAccessIterator en)
+{
+    for(RandomAccessIterator ptr  = begi; ptr != en; ptr++)
+        for(RandomAccessIterator j=ptr; j != begi && *(j-1)>*j;j--)
+			{
+			   std::swap(*(j-1),*j);
+            }
+}
